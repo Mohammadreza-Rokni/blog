e4e04a9593b2bec4de72b5d16380529676f0574a
soheila <mrokniahmadi.mr@gmail.com>
2022-07-23 18:42:49 +0430

    add login

--- blog/settings.py
+++ blog/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # my app
-    'home_app.apps.HomeAppConfig',
-    'account.apps.HomeAppConfig'
+    'home.apps.HomeAppConfig',
+    'account.apps.HomeAppConfig',
 ]
 
 MIDDLEWARE = [
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR ,"assets")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR ,"static")]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
